3581f3130d9b567de69bf0a23e1ae20e599d937e
Fern Aerell <fernaerell2020@gmail.com>
2025-12-29 16:24:13 +0700

    feat: Add hello_aerell.c

---
+++ hello_aerell.c
@@ -0,0 +1,14 @@
+// Example to print "Hello Aerell!" in Python.
+
+// Include standard library for printf function
+#include <stdio.h>
+
+// Create entry point function
+int main() {
+    
+    // Call printf function with argument "Hello Aerell!"
+    printf("Hello Aerell!");
+
+    // Don't forget for return int, 0 it mean's success
+    return 0;
+}
