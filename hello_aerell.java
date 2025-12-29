8b76738b2f587982d909b182abd14563d0fbaaf2
Fern Aerell <fernaerell2020@gmail.com>
2025-12-29 16:42:34 +0700

    feat: Add hello_aerell.java

---
+++ hello_aerell.java
@@ -0,0 +1,12 @@
+// Example to print "Hello Aerell!" in Java.
+
+// Create a class matching the filename
+public class Main {
+    
+    // Create entry point method
+    public static void main(String[] args) {
+        
+        // Call System.out.print with argument "Hello Aerell!"
+        System.out.print("Hello Aerell!");
+    }
+}
