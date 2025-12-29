5a914d0b4af46e48dfe228e9a591fcc88ad495f1
Fern Aerell <fernaerell2020@gmail.com>
2025-12-29 16:30:48 +0700

    feat: Add hello_aerell.cpp

---
+++ hello_aerell.cpp
@@ -0,0 +1,14 @@
+// Example to print "Hello Aerell!" in C++.
+
+// Include iostream for standard input/output stream
+#include <iostream>
+
+// Create entry point function
+int main() {
+    
+    // Call std::cout to output "Hello Aerell!"
+    std::cout << "Hello Aerell!";
+
+    // Don't forget for return int, 0 it mean's success
+    return 0;
+}
