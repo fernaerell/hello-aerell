aa678094ce7e9aaa89d9d928d553b046f05e9c0c
Fern Aerell <fernaerell2020@gmail.com>
2025-12-29 16:34:13 +0700

    feat: Add hello_aerell.go

---
+++ hello_aerell.go
@@ -0,0 +1,14 @@
+// Example to print "Hello Aerell!" in Go.
+
+// Define the main package
+package main
+
+// Import fmt package for formatted I/O
+import "fmt"
+
+// Create entry point function
+func main() {
+    
+    // Call Printf function from fmt package
+    fmt.Printf("Hello Aerell!")
+}
