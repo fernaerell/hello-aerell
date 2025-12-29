98d50acda398f2ac3968721293d3c209b8726936
Fern Aerell <fernaerell2020@gmail.com>
2025-12-29 16:33:06 +0700

    feat: Add hello_aerell.cs

---
+++ hello_aerell.cs
@@ -0,0 +1,13 @@
+// Example to print "Hello Aerell!" in C#.
+
+// Use System namespace for Console class
+using System;
+
+// Main entry point for the program
+class Program {
+    static void Main() {
+        
+        // Call WriteLine method with argument "Hello Aerell!"
+        Console.WriteLine("Hello Aerell!");
+    }
+}
