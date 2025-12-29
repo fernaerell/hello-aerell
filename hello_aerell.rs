7a6827a24693873af2dce0c68b762f277d7e07c8
Fern Aerell <fernaerell2020@gmail.com>
2025-12-29 16:40:03 +0700

    feat: Add hello_aerell.rs

---
+++ hello_aerell.rs
@@ -0,0 +1,8 @@
+// Example to print "Hello Aerell!" in Rust.
+
+// Create entry point function
+fn main() {
+    
+    // Call println! macro with argument "Hello Aerell!"
+    println!("Hello Aerell!");
+}
