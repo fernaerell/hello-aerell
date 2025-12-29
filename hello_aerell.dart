ab7187eadffb0b1cd6545d269d436dc09ee42c56
Fern Aerell <fernaerell2020@gmail.com>
2025-12-29 16:43:18 +0700

    feat: Add hello_aerell.dart

---
+++ hello_aerell.dart
@@ -0,0 +1,8 @@
+// Example to print "Hello Aerell!" in Dart.
+
+// Create entry point function
+void main() {
+    
+    // Call print function with argument "Hello Aerell!"
+    print('Hello Aerell!');
+}
